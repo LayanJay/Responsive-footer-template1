51cd20b33235fc08566696c234223ac1fb575616
Layan <layanjayasinghe2@gmail.com>
2020-11-18 21:57:36 +0530

    fixes: minor bugs

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Container, Grid } from "@material-ui/core";
+import { Grid } from "@material-ui/core";
 import React from "react";
 import "./App.css";
 import logo from "./Assets/logo.svg";
@@ -7,6 +7,7 @@ function App() {
   return (
     <>
       <div className="container">
+        <div style={{ minHeight: "100vh" }}></div>
         <Grid container className="footer__container">
           <Grid container md={4} sm={12} alignItems="center" className="center">
             <img src={logo} alt="logo" className="logo" />
@@ -23,27 +24,27 @@ function App() {
             <Grid className="col" xs={12} sm md>
               <div className="col__title">Products</div>
               <div style={{ marginBottom: "0.4rem" }}>
-                <a href="#" className="col__links">
+                <a href="www.google.com" className="col__links">
                   Pricing
                 </a>
               </div>
               <div style={{ marginBottom: "0.4rem" }}>
-                <a href="#" className="col__links">
+                <a href="www.google.com" className="col__links">
                   Teams
                 </a>
               </div>
               <div style={{ marginBottom: "0.4rem" }}>
-                <a href="#" className="col__links">
+                <a href="www.google.com" className="col__links">
                   Education
                 </a>
               </div>
               <div style={{ marginBottom: "0.4rem" }}>
-                <a href="#" className="col__links">
+                <a href="www.google.com" className="col__links">
                   Refer a friend
                 </a>
               </div>
               <div style={{ marginBottom: "0.4rem" }}>
-                <a href="#" className="col__links">
+                <a href="www.google.com" className="col__links">
                   Updates
                 </a>
               </div>
@@ -51,27 +52,27 @@ function App() {
             <Grid className="col" xs={12} sm md>
               <div className="col__title">Features</div>
               <div style={{ marginBottom: "0.4rem" }}>
-                <a href="#" className="col__links">
+                <a href="www.google.com" className="col__links">
                   Overview
                 </a>
               </div>
               <div style={{ marginBottom: "0.4rem" }}>
-                <a href="#" className="col__links">
+                <a href="www.google.com" className="col__links">
                   Design
                 </a>
               </div>
               <div style={{ marginBottom: "0.4rem" }}>
-                <a href="#" className="col__links">
+                <a href="www.google.com" className="col__links">
                   Code
                 </a>
               </div>
               <div style={{ marginBottom: "0.4rem" }}>
-                <a href="#" className="col__links">
+                <a href="www.google.com" className="col__links">
                   Collaborate
                 </a>
               </div>
               <div style={{ marginBottom: "0.4rem" }}>
-                <a href="#" className="col__links">
+                <a href="www.google.com" className="col__links">
                   Sletch Plugin
                 </a>
               </div>
@@ -81,27 +82,27 @@ function App() {
             <Grid className="col" xs={12} sm md>
               <div className="col__title">Get Started</div>
               <div style={{ marginBottom: "0.4rem" }}>
-                <a href="#" className="col__links">
+                <a href="www.google.com" className="col__links">
                   Tutorials
                 </a>
               </div>
               <div style={{ marginBottom: "0.4rem" }}>
-                <a href="#" className="col__links">
+                <a href="www.google.com" className="col__links">
                   Resources
                 </a>
               </div>
               <div style={{ marginBottom: "0.4rem" }}>
-                <a href="#" className="col__links">
+                <a href="www.google.com" className="col__links">
                   Guides
                 </a>
               </div>
               <div style={{ marginBottom: "0.4rem" }}>
-                <a href="#" className="col__links">
+                <a href="www.google.com" className="col__links">
                   Examples
                 </a>
               </div>
               <div style={{ marginBottom: "0.4rem" }}>
-                <a href="#" className="col__links">
+                <a href="www.google.com" className="col__links">
                   Docs
                 </a>
               </div>
@@ -109,37 +110,37 @@ function App() {
             <Grid className="col" xs={12} sm md>
               <div className="col__title">About</div>
               <div style={{ marginBottom: "0.4rem" }}>
-                <a href="#" className="col__links">
+                <a href="www.google.com" className="col__links">
                   Stories
                 </a>
               </div>
               <div style={{ marginBottom: "0.4rem" }}>
-                <a href="#" className="col__links">
+                <a href="www.google.com" className="col__links">
                   Community
                 </a>
               </div>
               <div style={{ marginBottom: "0.4rem" }}>
-                <a href="#" className="col__links">
+                <a href="www.google.com" className="col__links">
                   Blog
                 </a>
               </div>
               <div style={{ marginBottom: "0.4rem" }}>
-                <a href="#" className="col__links">
+                <a href="www.google.com" className="col__links">
                   Careers
                 </a>
               </div>
               <div style={{ marginBottom: "0.4rem" }}>
-                <a href="#" className="col__links">
+                <a href="www.google.com" className="col__links">
                   Brand Assets
                 </a>
               </div>
             </Grid>
           </Grid>
           <Grid md={12} sm={12} className="social">
+            <hr className="social__hr" />
             <div>social tags//</div>
           </Grid>
         </Grid>
-        <hr />
       </div>
     </>
   );
